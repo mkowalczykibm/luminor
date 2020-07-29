3d7a9eb5bfd13e24cbbcbb957ca78ade288536fb
mkowalczykibm <m.kowalczyk@pl.ibm.com>
2020-07-29 13:59:04 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,28 +4,18 @@ variable "ibmcloud_region" {
 }
 
 variable "vpc_name" {
-  default = "vpc-fs2020-lab"
+  default = "luminorvpc"
   description = "Name of your VPC"
 }
 
 variable "zone1" {
-  default = "us-south-1"
+  default = "eu-de-1"
   description = "Define the 1st zone of the region"
-}
-
-variable "zone2" {
-  default = "us-south-2"
-  description = "Define the 2nd zone of the region"
 }
 
 variable "zone1_cidr" {
   default = "172.16.1.0/24"
   description = "CIDR block to be used for zone 1"
-}
-
-variable "zone2_cidr" {
-  default = "172.16.2.0/24"
-  description = "CIDR block to be used for zone 2"
 }
 
 variable "ssh_key_name" {
